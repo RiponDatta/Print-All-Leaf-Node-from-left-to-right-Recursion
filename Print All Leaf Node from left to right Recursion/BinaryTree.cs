638a0f09d6e661c66eaa977653857ba0dfb277fd
Ripon Datta <ripondatta.us@gmail.com>
2019-06-11 23:51:26 -0700

    Add project files.

---
+++ Print All Leaf Node from left to right Recursion/BinaryTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_All_Leaf_Node_from_left_to_right_Recursion
+{
+    public class BinaryTree
+    {
+        public Node Root;
+        private Queue<Node> Nodes;
+
+        public Queue<Node> GetLeafNodes()
+        {
+            Nodes = new Queue<Node>();
+            GetLeafNodes(Root);
+            return Nodes;
+        }
+
+        private void GetLeafNodes(Node node)
+        {
+            if(node==null)
+            {
+                return;
+            }
+            if(node.Left == null && node.Right == null)
+            {
+                Nodes.Enqueue(node);
+                return;
+            }
+            if(node.Left != null)
+            {
+                GetLeafNodes(node.Left);
+            }
+            if (node.Right != null)
+            {
+                GetLeafNodes(node.Right);
+            }
+        }
+
+        public void PrintNode(Queue<Node> nodes)
+        {
+            while (nodes.Count > 0)
+            {
+                var current = nodes.Dequeue();
+                Console.Write(current.Data + " ");
+            }
+        }
+    }
+}
---
+++ Print All Leaf Node from left to right Recursion/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_All_Leaf_Node_from_left_to_right_Recursion
+{
+    public class Node
+    {
+        public int Data;
+        public Node Left, Right;
+
+        public Node(int data)
+        {
+            Data = data;
+            Left = Right = null;
+        }
+    }
+}
